9284cc72e357f071ac032279e075cb2e701d4d3a
KLaszlo24 <krajczlaci234@gmail.com>
2025-02-14 11:51:34 +0100

    Első és a második feladat kész, a legnagyobb életerővel és a listával, beolvasással

--- fájlkezelés/Program.cs
+++ fájlkezelés/Program.cs
@@ -4,7 +4,51 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			List<Karakter> karakterek = [];
+			Beolvasas("karakterek.txt", karakterek);
+			//bin --> debug --> net8
+
+			foreach(var item in karakterek)
+			{
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("2. feladat");
+
+            Legtobb(karakterek);
+		}
+
+		static void Beolvasas(string fajlnev, List<Karakter> karakterek )
+		{
+			StreamReader sr = new(fajlnev);  //be tud olvasni fájlokat, szövegeket
+
+			string[] sorok = File.ReadAllLines(fajlnev);
+
+			sr.ReadLine();
+
+			while (!sr.EndOfStream)
+			{
+				string sor = sr.ReadLine();
+				string[] szavak = sor.Split(";");
+
+				Karakter karakter = new Karakter(szavak[0], Convert.ToInt16(szavak[1]), Convert.ToInt16(szavak[2]), Convert.ToInt16(szavak[3]));
+				karakterek.Add(karakter);
+			}
+
+			
+		}
+		static void Legtobb(List<Karakter> karakterek)
+		{
+			Karakter valtozo = karakterek[0];
+			for (int i = 1; i < karakterek.Count; i++)
+			{
+				if (valtozo.Eletero < karakterek[i].Eletero)
+				{
+					valtozo = karakterek[i];
+				}
+			}
+				Console.WriteLine($"Legmagasabb életerővel rendelkező karakter: {valtozo.Nev}, {valtozo.Szint}, {valtozo.Ero}");
 		}
 	}
 }
